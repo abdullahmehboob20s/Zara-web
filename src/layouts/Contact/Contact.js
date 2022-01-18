e7c9dd1cfd3af490dd2c15e3f78435ac6b3c7b3b
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-01-18 20:43:05 +0500

    done

--- src/layouts/Contact/Contact.js
+++ src/layouts/Contact/Contact.js
@@ -5,9 +5,10 @@ function Contact() {
   return (
     <div className="container-wrapper py-70px">
       <p className="gray fs-20px lh-1_6 weight-5  text-center">
-        Come and experience Africa in a whole new way and snag your 10% Gift
-        Voucher and our stunning LookBook."
-        {/* <span className="weight-8">worldwide delivery</span>. */}
+        We are a full-service online African store offering sustainable quality
+        products from the finest brands in Africa, carefully curated for you.
+        ZaharaMall partners with STRIPE to ensure secure payments & with DHL for{" "}
+        <span className="weight-8">worldwide delivery</span>.
       </p>
     </div>
   );
