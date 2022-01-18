f544d1d07b13f7902e40b14087718ec0bb4cdf80
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-01-18 20:31:52 +0500

    done

--- src/layouts/Contact/Contact.js
+++ src/layouts/Contact/Contact.js
@@ -5,10 +5,9 @@ function Contact() {
   return (
     <div className="container-wrapper py-70px">
       <p className="gray fs-20px lh-1_6 weight-5  text-center">
-        We are a full-service online African store offering sustainable quality
-        products from the finest brands in Africa, carefully curated for you.
-        ZaharaMall partners with STRIPE to ensure secure payments & with DHL for{" "}
-        <span className="weight-8">worldwide delivery</span>.
+        Come and experience Africa in a whole new way and snag your 10% Gift
+        Voucher and our stunning LookBook."
+        {/* <span className="weight-8">worldwide delivery</span>. */}
       </p>
     </div>
   );
--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -14,8 +14,18 @@ function Footer() {
       <div className="container-wrapper py-50px">
         <div className={styles.footer}>
           <div className={styles.footerLeft}>
-            <h1 className="fs-18px weight-6 lh-1_6">
-              ZaharaMall Copyright@2022 | Privacy Policy | Terms & Conditions
+            <h1 className="fs-18px weight-6 lh-1_6 gray">
+              ZaharaMall Copyright@2022 |{" "}
+              <a className="gray" href="https://zaharamall.com/privacy-policy">
+                Privacy Policy
+              </a>{" "}
+              |{" "}
+              <a
+                className="gray"
+                href="https://zaharamall.com/terms-conditions"
+              >
+                Terms & Conditions
+              </a>
             </h1>
           </div>
           <div className={styles.footerRight}>
